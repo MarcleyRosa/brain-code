809e802ea070650810a77faad3000e7e27f72234
Igor Kevin <igor_kevin@hotmail.com>
2025-02-07 15:10:31 -0300

    fix: return e nao log!

--- src/checkLicenseEligibility.js
+++ src/checkLicenseEligibility.js
@@ -1,11 +1,10 @@
 function checkLicenseEligibility(age, yearsOfExperience) {
   if (age<18) {
-    console.log();
-    
-  } if (age>18 && yearsOfExperience<2) {
-    console.log("Pode obter a licença, mas é necessária supervisão.");
+    return("Não pode obter a licença.");
+  } else if (age>=18 && yearsOfExperience<2) {
+    return("Pode obter a licença, mas é necessária supervisão.");
   } else {
-    console.log("Pode obter a licença sem supervisão.");
+    return("Pode obter a licença sem supervisão.");
   }
 }
 
