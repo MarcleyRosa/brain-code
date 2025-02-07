1f6a896cccacaacca21f11e9f455991788216ab3
Igor Kevin <igor_kevin@hotmail.com>
2025-02-07 15:31:31 -0300

    add func printinnerelements

--- src/printInnerElements.js
+++ src/printInnerElements.js
@@ -1,7 +1,25 @@
 function printInnerElements(array) {
-  // escreva seu código aqui
+  if (array.length<3){
+    console.log('Tamanho do array inválido');
+    return('Tamanho do array inválido')    
+  }
+  else {
+    var lista = []
+    for (let index = 1; index < (array.length-1); index++) {
+      lista.push(array[index])
+    }
+    for (let index = 0; index < lista.length; index++) {
+      console.log(lista[index]);
+    }
+  }
 }
+
 
 module.exports = {
   printInnerElements
 }
+
+var x = [1,2,32,5,2356,54,7]
+console.log(printInnerElements(x));
+console.log(printInnerElements(['asf', 'asvsdh', 'dfgsfgdhghjh', 'asdhjkdfhfgd', 'sdf']))
+console.log(printInnerElements([1,2]))
