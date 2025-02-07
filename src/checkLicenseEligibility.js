bd62c4eb1f22671b54e9e95c9fd0eb3b669a8d76
Igor Kevin <igor_kevin@hotmail.com>
2025-02-07 14:54:43 -0300

    add func: checkLicence

--- src/checkLicenseEligibility.js
+++ src/checkLicenseEligibility.js
@@ -1,5 +1,12 @@
 function checkLicenseEligibility(age, yearsOfExperience) {
-  // escreva seu código aqui
+  if (age<18) {
+    console.log();
+    
+  } if (age>18 && yearsOfExperience<2) {
+    console.log("Pode obter a licença, mas é necessária supervisão.");
+  } else {
+    console.log("Pode obter a licença sem supervisão.");
+  }
 }
 
 module.exports = { 
