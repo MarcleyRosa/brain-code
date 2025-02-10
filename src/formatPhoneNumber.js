a146ef7f4a0591cea993d90dd21dc1d31eb342b7
Igor Kevin <igor_kevin@hotmail.com>
2025-02-10 15:56:13 -0300

    add func formatPhoneNumber

--- src/formatPhoneNumber.js
+++ src/formatPhoneNumber.js
@@ -1,7 +1,50 @@
 function formatPhoneNumber(telNumber) {
-  // escreva seu código aqui
+  
+  // ddd
+  telFormat = '('
+  if (telNumber.length != 11){
+    return 'Array com tamanho incorreto.'
+  }
+  count = [0,0,0,0,0,0,0,0,0,0]
+  for (let index = 0; index < 2; index++) {
+    if (telNumber[index]>9 || telNumber[index]<0){
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+    telFormat += telNumber[index];
+    count[telNumber[index]] += 1
+  }
+
+  telFormat += ') '
+
+  // primeiros números
+  for (let index = 2; index < 7; index++) {
+    if (telNumber[index]>9 || telNumber[index]<0){
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+    telFormat += telNumber[index];
+    count[telNumber[index]] += 1
+    if (count[telNumber[index]] >= 3){
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+  }
+  telFormat += '-'
+
+  // ultimos números
+  for (let index = 7; index < 11; index++) {
+    if (telNumber[index]>9 && telNumber[index]<0){
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+    telFormat += telNumber[index];
+    count[telNumber[index]] += 1
+    if (count[telNumber[index]] >= 3){
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+  }
+  return telFormat
 }
 
+
+console.log(formatPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 module.exports = {
     formatPhoneNumber
 }
