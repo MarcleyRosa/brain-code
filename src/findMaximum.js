a2cf3ad29604acc13667b26c80a2a9bbd54a0768
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2025-05-09 17:14:49 -0300

    findMaximum.js

--- src/findMaximum.js
+++ src/findMaximum.js
@@ -1,7 +1,17 @@
 function findMaximum(array) {
-  // escreva seu código aqui
+  let maior = array[0];
+  for(let i=0; i < array.length; i++){
+    if(array[i] > maior){
+      maior = array[i];
+    }
+  }
+  return maior;
 }
 
+//const array = [-1, -5, -3, -2];
+//const maxNumber = findMaximum(array);
+//console.log(maxNumber); 
+
 module.exports = {
-  findMaximum
+ findMaximum
 }
