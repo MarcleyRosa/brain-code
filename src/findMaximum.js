7fbbdd1afea14602acf3ec4bc99f6478a0997077
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2025-05-15 13:13:38 -0300

    findMaximum done

--- src/findMaximum.js
+++ src/findMaximum.js
@@ -1,5 +1,13 @@
 function findMaximum(array) {
-  // escreva seu código aqui
+  let num = array[0];
+  for(let i = 0; i <= (array.length - 1); i += 1){
+    if(array.length === 0){
+      return undefined;
+    } else if(array[i] > num){
+      num = array[i];
+    }
+  }
+  return num;
 }
 
 module.exports = {
