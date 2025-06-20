0cf6520fdea4c243095b5643ddcbb89a0ce61973
Edson Copque <ecop@disroot.org>
2025-06-20 18:08:37 -0300

    feat: add complete solutions for brain-code challenges.

--- src/checkLicenseEligibility.js
+++ src/checkLicenseEligibility.js
@@ -1,7 +1,16 @@
 function checkLicenseEligibility(age, yearsOfExperience) {
   // escreva seu código aqui
+  if (age < 18) {
+      return 'Não pode obter a licença.';
+  } else if (age >= 18 && yearsOfExperience < 2) {
+      return 'Pode obter a licença, mas é necessária supervisão.';
+  } else if (age >= 18 && yearsOfExperience >= 2) {
+      return 'Pode obter a licença sem supervisão.';
+  } else {
+      return 'Bla blá!';
+  }
 }
-
 module.exports = { 
   checkLicenseEligibility
 }
+// Edson Copque - https://linktr.ee/edsoncopque
--- src/findMaximum.js
+++ src/findMaximum.js
@@ -1,7 +1,18 @@
 function findMaximum(array) {
   // escreva seu código aqui
+  if (array.length === 0) {
+      return undefined;
+  }
+  let maior_numero = array[0];
+  for (let i = 1; i < array.length; i++) {
+      if (array[i] > maior_numero) {
+          maior_numero = array[i];
+      }
+  }
+  return maior_numero;
 }
 
 module.exports = {
   findMaximum
 }
+// Edson Copque - https://linktr.ee/edsoncopque
