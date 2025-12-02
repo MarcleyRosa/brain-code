cdbc1108f6f532f5b64b4e58ded14424f0500c21
Thiago Giudice <tgiudice@gmail.com>
2025-12-02 19:57:03 -0300

    atualizando 06.

--- src/invertElements.js
+++ src/invertElements.js
@@ -1,12 +1,17 @@
 function invertElements(array) {
   let invertido = []
-  for (let index = 0; index < array.length; index++) {
-    invertido.unshift(array[index])
-    
-  }
+
+  for (let index = (array.length - 1); index >= 0 ; index--) {
+  invertido[(array.length - 1)-index] = array[index] 
+}
+  //for (let index = 0; index < array.length; index++) {
+  //  invertido.unshift(array[index])
+  //  
+  //}
   return invertido
 }
 
 module.exports = {
   invertElements
 }
+
